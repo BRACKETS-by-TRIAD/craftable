07f6b5ab222c2b806e27df84729baeaec78812a8
Pavol Perdík <palypster@gmail.com>
2017-08-30 18:11:19 +0200

    removing providers registration

--- src/SimplewebServiceProvider.php
+++ src/SimplewebServiceProvider.php
@@ -25,18 +25,6 @@ class SimplewebServiceProvider extends ServiceProvider
                 ], 'migrations');
             }
         }
-
-        // In Laravel 5.5 these all is going to be deprecated
-        $this->app->register(\Brackets\Admin\AdminServiceProvider::class);
-        $this->app->register(\Brackets\AdminListing\AdminListingServiceProvider::class);
-        // FIXME this should not be here, because this package is dev dependency, but it's going to be solved with auto-discovery in Laravel 5.5
-        $this->app->register(\Brackets\AdminGenerator\AdminGeneratorServiceProvider::class);
-        $this->app->register(\Brackets\AdminAuth\AdminAuthServiceProvider::class);
-        $this->app->register(\Brackets\AdminTranslations\AdminTranslationsServiceProvider::class);
-        $this->app->register(\Brackets\Media\MediaServiceProvider::class);
-        $this->app->register(\Brackets\Translatable\TranslatableServiceProvider::class);
-        $this->app->register(\Spatie\Permission\PermissionServiceProvider::class);
-        $this->app->register(\Spatie\Backup\BackupServiceProvider::class);
     }
 
     /**
