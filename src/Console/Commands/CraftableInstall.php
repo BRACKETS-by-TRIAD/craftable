8a48c4d0898bf43d05b0c52a4d9ff5eaef7f4c45
Pavol Perdík <palypster@gmail.com>
2019-09-19 23:39:06 +0200

    Fixing missing translations from admin-ui and admin-translations packages

--- src/Console/Commands/CraftableInstall.php
+++ src/Console/Commands/CraftableInstall.php
@@ -178,7 +178,9 @@ class CraftableInstall extends Command
             '// here you can add your own directories
         // base_path(\'routes\'), // uncomment if you have translations in your routes i.e. you have localized URLs
         base_path(\'vendor/brackets/admin-auth/src\'),
-        base_path(\'vendor/brackets/admin-auth/resources\'),'
+        base_path(\'vendor/brackets/admin-auth/resources\'),
+        base_path(\'vendor/brackets/admin-ui/resources\'),
+        base_path(\'vendor/brackets/admin-translations/resources\'),'
         );
 
         $this->call('admin-translations:scan-and-save', [
